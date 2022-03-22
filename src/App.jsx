9496b83bfc480d375a6bfffc1d504efbc5edbd3e
Rodrigo Marcolino <rodrigomarcolino@gmail.com>
2022-03-22 16:38:04 +1300

    restart project from zero

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import Typography from '@mui/material/Typography'
-import AppBar from '@mui/material/AppBar'
-import Card from '@mui/material/Card'
-import CardActions from '@mui/material/CardActions'
-import CardContent from '@mui/material/CardContent'
-import CardMedia from '@mui/material/CardMedia'
-import CssBaseline from '@mui/material/CssBaseline'
-import Grid from '@mui/material/Grid'
-import Toolbar from '@mui/material/Toolbar'
-import CameraIcon from '@mui/icons-material/PhotoCamera'
 
 const App = () => {
-  return (<> 
-  <CssBaseline />
-  
-  </>)
-
+  return (
+    <>
+      <h1>reset</h1>
+    </>
+  )
+}
 export default App
