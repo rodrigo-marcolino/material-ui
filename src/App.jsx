abff98deea1d872b99065731bcc0d93e7b62fe72
Rodrigo Marcolino <rodrigomarcolino@gmail.com>
2022-03-24 15:28:06 +1300

    create AppBarMenu

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './app.css'
 import PlaceCard from './components/PlaceCard'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
+import AppBarMenu from './components/AppBarMenu'
 
 const App = () => {
   return (
     <>
-      <Container>
+      <AppBarMenu />
+      <Container sx={{ marginY: 5 }}>
         <Grid container spacing={3}>
           <PlaceCard />
           <PlaceCard />
